8aa1a3a8c577ca07d5e2d72bfa4a2771f35bb515
Kevin Kevin <kevinmfranklin@students.abtech.edu>
2024-08-22 15:36:51 -0400

    Bird challenge completed.

--- asgn01/bird-challenge/index.php
+++ asgn01/bird-challenge/index.php
@@ -17,3 +17,23 @@ class Bird
     return $this->commonName . " is at a conservation level of " . $this->conservationLevel;
   }
 }
+
+$bird1 = new Bird;
+$bird1->commonName = 'Eastern Towhee';
+$bird1->food = 'seeds, fruits, insects, and spiders';
+$bird1->nestPlacement = 'ground';
+$bird1->conservationLevel = 'Low';
+
+$bird2 = new Bird;
+$bird2->commonName = 'Indigo Bunting';
+$bird2->food = 'small seeds, berries, buds, and insects';
+$bird2->nestPlacement = 'roadsides';
+$bird2->conservationLevel = 'Low';
+
+echo $bird1->commonName . "<br>";
+echo $bird1->diet() . "<br>";
+echo $bird1->concern() . "<br><br>";
+
+echo $bird2->commonName . "<br>";
+echo $bird2->diet() . "<br>";
+echo $bird2->concern() . "<br><br>";
