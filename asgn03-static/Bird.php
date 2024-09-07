f1097a354d8d3513f113410eb27a778ee92b5de4
Kevin Kevin <kevinmfranklin@students.abtech.edu>
2024-09-07 11:54:26 -0400

    Finished working on Bird.php. Starting work on index.php.

--- asgn03-static/Bird.php
+++ asgn03-static/Bird.php
@@ -1,31 +1,40 @@
 <?php
 
-class Bird {
-    var $habitat;
-    var $food;
-    var $nesting = "tree";
-    var $conservation;
-    var $song = "chirp";
-    var $flying = "yes";
+class Bird
+{
+  protected $name;
+  protected $habitat;
+  protected $diet;
+  protected $nesting = "tree";
+  protected $conservation;
+  protected $song = "chirp";
+  protected static $flying = "yes";
+  private static $instanceCount;
+  protected static $eggNum = 0;
 
-    function can_fly() {
-        if ( $this->flying == "yes" ) {
-            $flying_string = "can fly";
-        } else {
-            $flying_string = "is stuck on the ground";
-        }
-        return  $flying_string ;
-    }
+  public static function create()
+  {
+    self::$instanceCount++;
+    return new self();
+  }
+
+  public static function can_fly()
+  {
+    return static::$flying == "yes" ? " can fly" : " cannot fly and is stuck on the ground";
+  }
 }
 
-class YellowBelliedFlyCatcher extends Bird {
-    var $name = "yellow-bellied flycatcher";
-    var $diet = "mostly insects.";
-    var $song = "flat chilk";
+class YellowBelliedFlyCatcher extends Bird
+{
+  public $name = "yellow-bellied flycatcher";
+  public $diet = "mostly insects";
+  public $song = "flat chilk";
+  protected static $eggNum = "3-4, sometimes 5.";
 }
 
-class Kiwi extends Bird {
-    var $name = "kiwi";
-    var $diet = "omnivorous";
-    var $flying = "no";
+class Kiwi extends Bird
+{
+  public $name = "kiwi";
+  public $diet = "omnivorous";
+  public $flying = "no";
 }
