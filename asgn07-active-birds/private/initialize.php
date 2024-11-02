d5f4508cd43360ec19fb3ce904361bb21ebf5ae8
Kevin Kevin <kevinmfranklin@students.abtech.edu>
2024-11-02 18:49:22 -0400

    Finished asgn07 active birds.

--- asgn07-active-birds/private/initialize.php
+++ asgn07-active-birds/private/initialize.php
@@ -3,23 +3,33 @@ ob_start(); // turn on output buffering
 
 // session_start(); // turn on sessions if needed
 
+// Assign file paths to PHP constants
+// __FILE__ returns the current path to this file
+// dirname() returns the path to the parent directory
 define("PRIVATE_PATH", dirname(__FILE__));
 define("PROJECT_PATH", dirname(PRIVATE_PATH));
 define("PUBLIC_PATH", PROJECT_PATH . '/public');
 define("SHARED_PATH", PRIVATE_PATH . '/shared');
 
+// Assign the root URL to a PHP constant
+// * Do not need to include the domain
+// * Use same document root as webserver
+// * Can set a hardcoded value:
+// define("WWW_ROOT", '/~kevinskoglund/chain_gang/public');
+// define("WWW_ROOT", '');
+// * Can dynamically find everything in URL up to "/public"
 $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("WWW_ROOT", $doc_root);
 
-
 require_once('functions.php');
+require_once('db_credentials.php');
+require_once('database_functions.php');
 
-// Require all necessary classes
+// Load class definitions manually
 foreach (glob('classes/*.class.php') as $file) {
   require_once($file);
 }
-
 
 // Autoload class definitions
 function my_autoload($class)
@@ -29,3 +39,6 @@ function my_autoload($class)
   }
 }
 spl_autoload_register('my_autoload');
+
+$database = db_connect();
+Bird::set_database($database);
